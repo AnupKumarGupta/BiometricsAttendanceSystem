6bc33143a8c17d9ed75750f14535062a915df17c
GuptaAnupKumar <reachanupgupta@gmail.com>
2016-03-03 13:09:34 +0530

    Connection Class Added

---
+++ biometric-attendance-system/Admin/AddSession.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPages/Admin.master" AutoEventWireup="true" CodeFile="AddSession.aspx.cs" Inherits="Admin_AddSession" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+<div class="row">
+        <div class="col s12 breadcrumb">
+            <br />
+            <a href="~/Admin/ManageMasterEntries.aspx" class="grey-text">Home &nbsp;&nbsp;></a>
+            <a href="#!" class="teal-text">&nbsp;&nbsp;Add Session &nbsp;&nbsp;</a>
+        </div>
+        <div class="col s4 m4 l4 offset-s1 offset-m1 offset-l1 card">
+            <asp:GridView ID="grdSession" runat="server" AutoGenerateColumns="false" CssClass="responsive-table striped" EmptyDataText="No Data">
+                <Columns>
+                    <asp:TemplateField>
+                        <HeaderTemplate>
+                            <asp:Label Text="Session Start Date" runat="server" />
+                        </HeaderTemplate>
+                        <ItemTemplate>
+                            <asp:Label ID="lblSessionStartDate" runat="server" Text='<%#Eval("SessionStartDate","{0:d}")%>'></asp:Label>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField>
+                        <HeaderTemplate>
+                            <asp:Label Text="SessionEndDate" runat="server" />
+                        </HeaderTemplate>
+                        <ItemTemplate>
+                            <asp:Label ID="lblSessionEndDate" runat="server" Text='<%#Eval("SessionEndDate","{0:d}")%>'></asp:Label>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                </Columns>
+            </asp:GridView>
+        </div>
+        <div class="col s4 m4 l4 offset-s1 offset-m1 offset-l1 card">
+            <asp:Button Text="Add Session" ID="btnAddSession"  OnClick="btnAddSession_Click" runat="server" />
+        </div>
+    </div>
+
+</asp:Content>
+
--- biometric-attendance-system/App_Code/ManageAttendance.cs
+++ biometric-attendance-system/App_Code/ManageAttendance.cs
@@ -6,12 +6,14 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using BAS;
 
 /// <summary>
 /// Summary description for ManageAttendance
 /// </summary>
 public class ManageAttendance
 {
+    #region Old Code
     //public int GetEntryStatus(Attendance objAttendance)
     //{
     //    DBDataHelper.ConnectionString = ConfigurationManager.ConnectionStrings["CSBiometricAttendance"].ConnectionString;
@@ -46,58 +48,66 @@ public class ManageAttendance
     //    return (String.IsNullOrEmpty(x)) ? 0 : 1;
     //}
 
-    public bool EntryOrExitEmployee(Attendance objAttendance, out int status)
-    {
-        DBDataHelper.ConnectionString = ConfigurationManager.ConnectionStrings["CSBiometricAttendance"].ConnectionString;
-        List<SqlParameter> lstAttendanceDetail = new List<SqlParameter>();
-        lstAttendanceDetail.Add(new SqlParameter("@employeeId", objAttendance.EmployeeId));
-        lstAttendanceDetail.Add(new SqlParameter("@dateTime", objAttendance.Date));
-        DataSet ds = new DataSet();
-        try
-        {
-            using (DBDataHelper objDDBDataHelper = new DBDataHelper())
-            {
-                ds = objDDBDataHelper.GetDataSet("spAssignAttendance", SQLTextType.Stored_Proc, lstAttendanceDetail);
-            }
-            status = Convert.ToInt32(ds.Tables[0].Rows[0][0]);
-            return true;
-        }
-        catch(Exception ex)
-        {
-            status = 0;
-            return false;
-        }
-    }
+    //public bool EntryOrExitEmployee(Attendance objAttendance, out int status)
+    //{
+    //    DBDataHelper.ConnectionString = ConfigurationManager.ConnectionStrings["CSBiometricAttendance"].ConnectionString;
+    //    List<SqlParameter> lstAttendanceDetail = new List<SqlParameter>();
+    //    lstAttendanceDetail.Add(new SqlParameter("@employeeId", objAttendance.EmployeeId));
+    //    lstAttendanceDetail.Add(new SqlParameter("@dateTime", objAttendance.Date));
+    //    DataSet ds = new DataSet();
+    //    try
+    //    {
+    //        using (DBDataHelper objDDBDataHelper = new DBDataHelper())
+    //        {
+    //            ds = objDDBDataHelper.GetDataSet("spAssignAttendance", SQLTextType.Stored_Proc, lstAttendanceDetail);
+    //        }
+    //        status = Convert.ToInt32(ds.Tables[0].Rows[0][0]);
+    //        return true;
+    //    }
+    //    catch(Exception ex)
+    //    {
+    //        status = 0;
+    //        return false;
+    //    }
+    //}
 
-    public List<Employees> GetEmployeesPresentDateWise(DateTime dateTime)
+    //public List<Employees> GetEmployeesPresentDateWise(DateTime dateTime)
+    //{
+    //    DBDataHelper.ConnectionString = ConfigurationManager.ConnectionStrings["CSBiometricAttendance"].ConnectionString;
+    //    List<Employees> lstEmployees = new List<Employees>();
+    //    List<SqlParameter> lstAttendanceDetail = new List<SqlParameter>();
+    //    lstAttendanceDetail.Add(new SqlParameter("@date", dateTime));
+    //    int i = 0;
+    //    DataSet ds = new DataSet();
+    //    try
+    //    {
+    //        using (DBDataHelper objDDBDataHelper = new DBDataHelper())
+    //        {
+    //            ds = objDDBDataHelper.GetDataSet("spGetEmployeesPresentDateWise", SQLTextType.Stored_Proc, lstAttendanceDetail);
+    //        }
+    //    }
+    //    catch (Exception ex)
+    //    {
+    //        return lstEmployees;
+    //    }
+    //    foreach (DataRow rows in ds.Tables[0].Rows)
+    //    {
+    //        Employees objEmployees = new Employees();
+    //        objEmployees.Id = Convert.ToInt32(ds.Tables[0].Rows[i][0]);
+    //        objEmployees.FirstName = ds.Tables[0].Rows[i][1].ToString();
+    //        objEmployees.MiddleName = ds.Tables[0].Rows[i][2].ToString();
+    //        objEmployees.LastName = ds.Tables[0].Rows[i][3].ToString();
+    //        lstEmployees.Add(objEmployees);
+    //        i++;
+    //    }
+    //    return lstEmployees;
+    //} 
+    #endregion
+
+    #region New Code
+    public void GetDataFromDevice()
     {
-        DBDataHelper.ConnectionString = ConfigurationManager.ConnectionStrings["CSBiometricAttendance"].ConnectionString;
-        List<Employees> lstEmployees = new List<Employees>();
-        List<SqlParameter> lstAttendanceDetail = new List<SqlParameter>();
-        lstAttendanceDetail.Add(new SqlParameter("@date", dateTime));
-        int i = 0;
-        DataSet ds = new DataSet();
-        try
-        {
-            using (DBDataHelper objDDBDataHelper = new DBDataHelper())
-            {
-                ds = objDDBDataHelper.GetDataSet("spGetEmployeesPresentDateWise", SQLTextType.Stored_Proc, lstAttendanceDetail);
-            }
-        }
-        catch (Exception ex)
-        {
-            return lstEmployees;
-        }
-        foreach (DataRow rows in ds.Tables[0].Rows)
-        {
-            Employees objEmployees = new Employees();
-            objEmployees.Id = Convert.ToInt32(ds.Tables[0].Rows[i][0]);
-            objEmployees.FirstName = ds.Tables[0].Rows[i][1].ToString();
-            objEmployees.MiddleName = ds.Tables[0].Rows[i][2].ToString();
-            objEmployees.LastName = ds.Tables[0].Rows[i][3].ToString();
-            lstEmployees.Add(objEmployees);
-            i++;
-        }
-        return lstEmployees;
+        new Connection().GetData();
     }
+    #endregion
 }
