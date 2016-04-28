0b5c626fc9743f97a3e63d976763e001e6444d1c
GuptaAnupKumar <reachanupgupta@gmail.com>
2016-04-28 12:34:49 +0530

    Connection Class Merged

---
+++ biometric-attendance-system/Admin/GetData.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class GetData : System.Web.UI.Page
+{
+    public zkemkeeper.CZKEMClass axCZKEM1 = new zkemkeeper.CZKEMClass();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btn_getData_Click(object sender, EventArgs e)
+    {
+        new Connection().GetData();
+    }
+}
+    
