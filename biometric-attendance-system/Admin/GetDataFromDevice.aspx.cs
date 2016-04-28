36d94376be77035f943e0a041577936e1cd8507f
GuptaAnupKumar <reachanupgupta@gmail.com>
2016-04-28 13:35:24 +0530

    Connection Module Completed

---
+++ biometric-attendance-system/Admin/GetDataFromDevice.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Admin_GetDataFromDevice : System.Web.UI.Page
+{
+    public zkemkeeper.CZKEMClass axCZKEM1 = new zkemkeeper.CZKEMClass();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btn_getData_Click(object sender, EventArgs e)
+    {
+        new Connection().GetData();
+    }
+}
