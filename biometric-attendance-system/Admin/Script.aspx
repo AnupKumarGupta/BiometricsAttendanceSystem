aad803549eb42bb78695a48849aadc9b950d2f8d
Aditi Singh <aditisinghsikarwar@gmail.com>
2016-05-10 11:34:45 +0530

    Script For Adding Leave Assigned Per Session

---
+++ biometric-attendance-system/Admin/Script.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" MasterPageFile="~/MasterPages/Admin.master" CodeFile="Script.aspx.cs" Inherits="Admin_Script" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
+    <script src="https://code.jquery.com/jquery-2.2.3.min.js" integrity="sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo=" crossorigin="anonymous"></script>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css" />
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+    <div class="row">
+        <div class="col s12 breadcrumb">
+            <br />
+            <a href="ManageMasterEntries.aspx" class="grey-text">Home &nbsp;&nbsp;></a>
+            <a href="#!" class="teal-text">&nbsp;&nbsp;Add Date Wise Shift &nbsp;&nbsp;</a>
+        </div>
+    </div>
+    
+    <div class="row">
+        <div class="col s8 m4 l4 offset-s2 offset-m5 offset-l5">
+            <asp:Button Text="Run Script" ID="btnAddSession" runat="server" CssClass="btn" OnClick="btnAddSession_Click" ValidationGroup="shift" />
+        </div>
+    </div>
+
+
+</asp:Content>
---
+++ biometric-attendance-system/Admin/Script.aspx.cs
@@ -0,0 +1,64 @@
+﻿using DALHelper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Admin_Script : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnAddSession_Click(object sender, EventArgs e)
+    {
+        #region Add_Session
+        DBDataHelper.ConnectionString = ConfigurationManager.ConnectionStrings["CSBiometricAttendance"].ConnectionString;
+
+        DateTime sessionStartDate = new DateTime(Int32.Parse(DateTime.Now.Year.ToString()), 8, 1);
+        DateTime sessionEndDate = new DateTime(sessionStartDate.Year + 1, 7, 31);
+
+        string query = "Select Count(*) from tblSession Where SessionStartDate=@sessionStartDate";
+        List<SqlParameter> lstParams = new List<SqlParameter>();
+        lstParams.Add(new SqlParameter("@sessionStartDate", sessionStartDate));
+        DataTable ds = new DataTable();
+        using (DBDataHelper objDDBDataHelper = new DBDataHelper())
+        {
+            ds = objDDBDataHelper.GetDataTable(query, SQLTextType.Query, lstParams);
+        }
+
+        if (ds.Rows.Count != 0)
+            if (Int32.Parse(ds.Rows[0][0].ToString()) == 0)
+            {
+
+                string addSession = @"INSERT INTO [dbo].[tblSession]
+                                      ([SessionStartDate]
+                                       ,[SessionEndDate])
+                                       VALUES (@sessionStartDate,@sessionEndDate)";
+
+                List<SqlParameter> lstParams2 = new List<SqlParameter>();
+                lstParams2.Add(new SqlParameter("@sessionStartDate", sessionStartDate));
+                lstParams2.Add(new SqlParameter("@sessionEndDate", sessionEndDate));
+
+                using (DBDataHelper objDDBDataHelper = new DBDataHelper())
+                {
+                    objDDBDataHelper.ExecSQL(addSession, SQLTextType.Query, lstParams2);
+                }
+                ManageReports objManageReprts = new ManageReports();
+                objManageReprts.AssignSessionWiseLeave(sessionStartDate);
+                objManageReprts.UpdateLeaveBalanceTable(sessionStartDate);
+                ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Session Added')", true);
+            }
+            else
+            {
+                ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Session Exists')", true);
+            }
+        #endregion
+    }
+}
